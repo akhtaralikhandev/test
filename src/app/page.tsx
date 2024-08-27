0846dcc8b9a6bead84df861c217ddb15e15a37b5
hammadkhanwazir <insafstudentpti@gmail.com>
2024-08-27 15:52:14 +0500

    loading state added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,35 @@
 "use client"
+import React, { useState, useEffect } from 'react';
 import MainSection from "@/components/main/main";
-export default function Home() {
+import Loading from '@/components/common/loading';
+
+const HomePage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const handlePageLoad = () => {
+      setIsLoading(false);
+    };
+
+    if (document.readyState === 'complete') {
+      handlePageLoad();
+    } else {
+      window.addEventListener('load', handlePageLoad);
+    }
+    return () => {
+      window.removeEventListener('load', handlePageLoad);
+    };
+  }, []);
+
   return (
-    <div>
-      <MainSection />
-    </div>
+    <>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <MainSection />
+      )}
+    </>
   );
-}
+};
+
+export default HomePage;
